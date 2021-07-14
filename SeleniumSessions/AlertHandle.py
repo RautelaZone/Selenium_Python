943a1c1b3a0497c92f7a4bcc8de17a958db07da5
rautelazone <anilrautela051090@gmail.com>
2021-07-14 20:10:06 +0530

    Checking

--- SeleniumSessions/AlertHandle.py
+++ SeleniumSessions/AlertHandle.py
@@ -16,6 +16,6 @@ driver.switch_to.default_content()  #back to main page
 driver.find_element_by_id("login1").send_keys("anyusername")
 time.sleep(3)
 # alert.dismiss() #for dismiss
-# alert.send_keys("") #for entering value
+alert.send_keys("") #for entering value
 
 driver.quit()
